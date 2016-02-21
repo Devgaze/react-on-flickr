4f7891b68c643c7e1a15a2d10d36dfc9306455db
Devgaze <alen@devgaze.com>
2016-02-22 00:11:12 +0100

    setup of project is done

---
+++ app/components/Main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+
+var React = require('react');
+var ReactDom = require('react-dom');
+
+
+var Main = React.createClass({
+
+  render: function(){
+
+    return (
+
+      <div>Hello World</div>
+
+    )
+
+  }
+
+});
+
+ReactDom.render(<Main />, document.getElementById('app'));
+
+
